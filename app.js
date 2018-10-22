38ad31834066b5c6657e06c10473ac09b5028ed7
Elias Andri <eliasandriasgeirsson@gmail.com>
2018-10-22 17:00:56 +0000

    Changed some files so that the app uses static files in the dist folder.

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const app = express();
 const api = require("./src/server/api");
+const path = require("path");
 //const PORT = process.env.PORT || 3000;
-
 
 app.use("/api", api);
 
-
-
-
+// Serve static files
+app.use(express.static(path.join(__dirname, "./", "dist")));
 
 app.get("*", (req, res) => {
     res.send({
         error: "No routine defined"
     });
 });
+
+// Prettify the JSON responses (from the API)
+app.set("json spaces", 2);
 
 module.exports = app;
