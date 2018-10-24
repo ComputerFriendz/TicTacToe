920ef0adfbc4df1794457bb40f7c5fc63326da3d
haukur <haukur16@ru.is>
2018-10-24 16:47:32 +0000

    Functions and tests added that loops over all collums and rows

--- src/logic/Board.js
+++ src/logic/Board.js
@@ -1,9 +1,26 @@
 import _ from 'lodash';
+import $ from 'jquery';
 
 const PLAYER_1_TOKEN = 'O';
 const PLAYER_2_TOKEN = 'X';
 
 class XOBoard {
 
+  constructor() {
+  }
+
+  eachCell(callback) {
+
+    _.forEach(this.board, (arr, row) => {
+
+
+      _.forEach(arr, (value, col) => {
+
+        if(callback) callback(value, row, col);
+      });
+    });
+  }
+
+  
 }
 export default XOBoard;
