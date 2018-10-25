7f6c6b2d2ce55bd8c7e422ce1ff744eeb83ecf0c
Elias Andri <eliasandriasgeirsson@gmail.com>
2018-10-25 16:09:48 +0000

    Implemented the findWinner function that takes a row, column and a player as a parameter and checks whether there is a winner.

--- src/logic/Board.js
+++ src/logic/Board.js
@@ -54,5 +54,43 @@ class XOBoard {
   $('#cell-' + row + '-' + col).html(token).attr('disabled', true);
   return this.board[row][col];
   }
+
+  findWinner(row, col, player) {
+    var num,
+      rowWin = true,
+      columnWin = true,
+      forwardDiagonalWin = true,
+      backwardDiagonalWin = true,
+      onDiagonal = (row === col) || (col == -1 * row + 2);
+
+    // check for row and column wins
+    for (num = 0; num < 3; num++) {
+      if (this.board[row][num] != player) {
+        rowWin = false;
+      }
+
+      if (this.board[num][col] != player) {
+        columnWin = false;
+      }
+    }
+
+    // check for diagonal wins
+    if (onDiagonal) {
+      for (num = 0; num < 3; num++) {
+        if (this.board[num][num] != player) {
+          forwardDiagonalWin = false;
+        }
+        if (this.board[num][-1 * num + 2] != player) {
+          backwardDiagonalWin = false;
+        }
+      }
+    } else {
+      forwardDiagonalWin = false;
+      backwardDiagonalWin = false;
+    }
+
+    // determine if a win was found
+    return rowWin || columnWin || forwardDiagonalWin || backwardDiagonalWin;
+  }
 }
 export default XOBoard;
