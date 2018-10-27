7f50270c515c2423473e570715573b374045874f
gingijons <34031184+gingijons@users.noreply.github.com>
2018-10-27 21:58:26 +0000

    Update screenshot.test.js

--- screenshot.test.js
+++ screenshot.test.js
@@ -6,7 +6,7 @@ it("Test if site is active", (async () => {
   const tester = await ScreenTest();
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('http://localhost:5000');
+  await page.goto('https://google.com');
   await page.screenshot({path: 'screenshot.png'});
   const result = await tester(page, 'screenshot.png');
   await browser.close();
