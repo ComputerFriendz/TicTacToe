9544903ef0768a7d069755cee1946a02e5570f45
Thelma Rut <thelmaj16@ru.is>
2018-10-28 19:53:08 +0000

    changing the code in screenshot.test.js in order to speed up the testing process

--- screenshot.test.js
+++ screenshot.test.js
@@ -1,3 +1,5 @@
+import { TIMEOUT } from 'dns';
+
 const puppeteer = require('puppeteer');
 const ScreenTest = require('puppeteer-screenshot-tester');
 
@@ -6,14 +8,10 @@ it("Test if site is active", (async () => {
   const tester = await ScreenTest();
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://floating-depths-40711.herokuapp.com/');
-  //let baseUrl = 'https://floating-depths-40711.herokuapp.com/'
-  //await page.goto(baseUrl, {"waitUntil" : "networkidle0"});
+  await page.goto('https://floating-depths-40711.herokuapp.com/',{timeout: 0});
   const result = await tester(page, 'screenshot.png');
-  //await page.screenshot({path: 'screenshot.png'});
   await browser.close();
   expect(result).toBe(true);
-  //jest.setTimeout(10000);
 }))
 });
 
