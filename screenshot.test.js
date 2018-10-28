822c01da97f206b11f458a0c65329ba0aeee9321
Elias Andri <eliasandriasgeirsson@gmail.com>
2018-10-28 19:32:16 +0000

    Fixing waiting time for screenshot

--- screenshot.test.js
+++ screenshot.test.js
@@ -6,11 +6,11 @@ it("Test if site is active", (async () => {
   const tester = await ScreenTest();
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://floating-depths-40711.herokuapp.com/');
+  await page.goto('https://floating-depths-40711.herokuapp.com/', {waitUntill: 'networkidle0'});
   await page.screenshot({path: 'screenshot.png'});
   const result = await tester(page, 'screenshot.png');
   await browser.close();
   expect(result).toBe(true);
-  jest.setTimeout(30000);
+  //jest.setTimeout(30000);
 }))
 });
